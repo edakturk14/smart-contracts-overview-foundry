cc7e83b0f935616b0fa64d4ca2c4855805952297
Eda Akturk <edaakturk@Edas-MacBook-Air.local>
2023-03-07 08:25:42 -0700

    Update erc721 contract

--- src/ERC721NFT.sol
+++ src/ERC721NFT.sol
@@ -3,16 +3,22 @@ pragma solidity ^0.8.17;
 
 // Import the ERC721 contract from the OpenZeppelin library
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
 // Define the ERC721NFT contract as inheriting from ERC721
-contract ERC721NFT is ERC721 {
+contract ERC721NFT is ERC721, Ownable {
     // Constructor sets the name and symbol of the token, and mints a new token to the deployer's address
     constructor() ERC721("EDA ERC721 NFT", "EDANFT") {
         _mint(msg.sender, 1);
     }
 
+    // Override the tokenURI function to return the custom URI for your contract
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "https://www.jsonkeeper.com/b/75N9";
+    }
+
     // Function to mint a new token and assign it to the specified recipient address
-    function mint(address to, uint id) external {
+    function mint(address to, uint id) external onlyOwner{
         _mint(to, id);
     }
 }
