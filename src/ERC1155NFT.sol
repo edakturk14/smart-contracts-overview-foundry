2ad92872e11b6eeb988e8eaaa9cea75d45379f87
Eda Akturk <edaakturk@Edas-MacBook-Air.local>
2023-03-08 11:50:52 -0600

    update erc1155

--- src/ERC1155NFT.sol
+++ src/ERC1155NFT.sol
@@ -1,26 +1,22 @@
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyERC1155 is ERC1155, Ownable {
+contract ERC1155NFT is ERC1155, Ownable {
     uint256 public constant NFT_ID = 1;
     uint256 public constant TOKEN_ID = 2;
     mapping (address => bool) private _nftMinted;
 
     constructor() ERC1155("https://gateway.pinata.cloud/ipfs/QmTWkZ46JY8MmqRh9jL4VeppQuHcCrap1hLVEaNJawAzWV/{id}.json") {}
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
-        _mint(account, id, amount, data);
-    }
-
     function mintNFT(address account, bytes memory data) public onlyOwner {
         require(!_nftMinted[account], "NFT already minted");
         _nftMinted[account] = true;
-        mint(account, NFT_ID, 1, data);
+        _mint(account, NFT_ID, 1, data);
     }
 
     function mintToken(address account, uint256 amount, bytes memory data) public onlyOwner {
-        mint(account, TOKEN_ID, amount, data);
+        _mint(account, TOKEN_ID, amount, data);
     }
 }
--- test/ERC1155NFT.t.sol
+++ test/ERC1155NFT.t.sol
@@ -1,33 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/ERC1155NFT.sol";
 
-contract ERC1155NFTTest is Test {
-    ERC1155NFT private erc1155nft;
-    address private constant alice = address(0x1);
-
-    function setUp() public {
-        erc1155nft = new ERC1155NFT();
-    }
+contract TestERC1155NFT is Test {
+    ERC1155NFT erc1155nft;
+    address owner = address(this);
+    address account1 = address(0x01);
+    address account2 = address(0x02);
+    bytes data = "0x01";
 
     function testMintNFT() public {
-        erc1155nft.mintNFT(alice);
-        uint256 balance = erc1155nft.balanceOf(alice, erc1155nft.getNFTId());
-        assertEq(balance, 1, "NFT balance should be 1");
+        erc1155nft = new ERC1155NFT();
+        erc1155nft.mintNFT(account1, data);
+        uint256 NFTId = 1;
+        assertEq(erc1155nft.balanceOf(account1, NFTId), 1, "NFT balance is incorrect");
     }
 
-    function testBurnNFT() public {
-        erc1155nft.mintNFT(address(this));
-        erc1155nft.burnNFT(address(this));
-        uint256 balance = erc1155nft.balanceOf(address(this), erc1155nft.getNFTId());
-        assertEq(balance, 0, "NFT balance should be 0");
-    }
-
-    function testTokenURI() public {
-        string memory expectedURI = "https://jsonkeeper.com/b/2KQZ";
-        string memory uri = erc1155nft.uri(erc1155nft.getTokenId());
-        assertEq(uri, expectedURI, "URI should match expected value");
+    function testMintToken() public {
+        erc1155nft = new ERC1155NFT();
+        uint256 amount = 100;
+        uint256 tokenId = 2;
+        erc1155nft.mintToken(account1, amount, data);
+        assertEq(erc1155nft.balanceOf(account1, tokenId), amount, "Token balance is incorrect");
     }
 }
